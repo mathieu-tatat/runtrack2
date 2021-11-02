fe00eccd09d140ea5c03745eeb3b93fcc6ea7f2f
mathieu-tatat <mathieu.tatat&laplateforme.io>
2021-11-02 16:27:11 +0100

    rectif job03

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -2,9 +2,50 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="index.css">
   <title>index</title>
 </head>
+<header>
+  <table>
+    <tr>
+      <td>TYPE</td>
+      <td>NOM</td>
+      <td>VALEUR</td>
+    </tr>
+  </table>
+  <br>
+</header>
 <body>
+  <table>
+
+    <tr>
+      <td>string</td>
+      <td>str</td>
+      <td>"bonjour a tous"</td>
+
+    </tr>
+    <tr>
+      <td>integer</td>
+      <td>int</td>
+      <td>5</td>
+
+    </tr>
+    <tr>
+      <td>boolean</td>
+      <td>bool</td>
+      <td>true</td>
+
+    </tr>
+    <tr>
+      <td>float</td>
+      <td>float</td>
+      <td>2.8</td>
+
+    </tr>
+  </table>
 
 </body>
+<footer>
+  <p>mon premier jour de php</p>
+</footer>
 </html>
