9d3ec137a83da9864cf29860ba11e967d905bea1
mathieu-tatat <mathieu.tatat&laplateforme.io>
2021-11-03 13:32:09 +0100

    rectifs job05

--- jour02/job05/job05.php
+++ jour02/job05/job05.php
@@ -1,21 +1,15 @@
-<?php  
-    $prime = 0;  
+<?php    
     $number = 2;  
-    while ($prime <=1000 )  
-    {  
-    $conteur=0;  
-    for ( $i=1; $i<=$number; $i++)  
-    {  
-    if (($number%$i)==0)  
-    {  
-    $conteur++;  
-    }  
-    }  
-    if ($conteur<3)  
-    {  
-    echo $number."<br>";  
-    $prime+=1;  
-    }  
-    $number+=1;  
+    while ($prime <=1000 ) {   
+        for ( $var=1; $var<=$number; $var+=1) {  
+            if (($number%$i)==0) {  
+        $conteur+=1;  
+            }  
+        }  
+        if ($conteur<3) {  
+            echo $number."<br>";  
+            $prime+=1;  
+            }  
+        $number+=1;  
     }  
     ?>  
