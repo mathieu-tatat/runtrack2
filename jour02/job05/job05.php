daedff0107f0472271b264a7dfdc109a16846b9f
mathieu-tatat <mathieu.tatat&laplateforme.io>
2021-11-03 13:49:38 +0100

    job05 final

--- jour02/job05/job05.php
+++ jour02/job05/job05.php
@@ -1,15 +1,21 @@
-<?php    
+<?php  
+    $prime = 0;  
     $number = 2;  
-    while ($prime <=1000 ) {   
-        for ( $var=1; $var<=$number; $var+=1) {  
-            if (($number%$i)==0) {  
-        $conteur+=1;  
-            }  
-        }  
-        if ($conteur<3) {  
-            echo $number."<br>";  
-            $prime+=1;  
-            }  
-        $number+=1;  
+    while ($prime <=1000 )  
+    {  
+    $conteur=0;  
+    for ( $var=1; $var<=$number; $var++)  
+    {  
+    if (($number%$var)==0)  
+    {  
+    $conteur++;  
+    }  
+    }  
+    if ($conteur<3)  
+    {  
+    echo $number."<br>";  
+    $prime+=1;  
+    }  
+    $number+=1;  
     }  
     ?>  
