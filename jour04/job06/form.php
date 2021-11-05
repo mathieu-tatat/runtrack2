c7bff9ea1d16fe0f7df974c6e9a29245946886ac
mathieu-tatat <mathieu.tatat&laplateforme.io>
2021-11-05 13:23:35 +0100

    add job06

---
+++ jour04/job06/form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+
+
+<?php
+if($_GET['nombre'] % 2 == 0 ){
+echo 'Nombre paire';}
+else{
+echo "Nombre impaire";
+}
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ jour04/job06/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+
+
+<?php
+
+?>
+
+<form action="form.php" method="get">
+ <p>nombre: <input type="text" name="nombre" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+
+
+
+
+
+</body>
+</html>
