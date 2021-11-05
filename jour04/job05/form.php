d6755db1a06a87ffb00977bcd3a601edd735a000
mathieu-tatat <mathieu.tatat&laplateforme.io>
2021-11-05 13:03:16 +0100

    add job05

---
+++ jour04/job05/form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+
+
+<?php
+if($_POST['username'] == "John" && $_POST['password'] == "Rambo"){
+echo 'C’est pas ma guerre';}
+else{
+echo "Votre pire cauchemard";
+}
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ jour04/job05/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+
+
+<?php
+
+?>
+
+<form action="form.php" method="post">
+ <p>UserName: <input type="text" name="username" /></p>
+ <p>PassWord: <input type="text" name="password" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+
+
+
+
+
+</body>
+</html>
