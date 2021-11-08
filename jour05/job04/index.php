88b17fbc0ce2f6dddb89836a33fa5fd7913b246f
mathieu-tatat <mathieu.tatat&laplateforme.io>
2021-11-08 11:10:38 +0100

    add job04

---
+++ jour05/job04/index.php
@@ -0,0 +1,23 @@
+<?php
+
+function calcule($a,$operation,$b) {
+    if ( $operation == '+'){
+        $result = $a + $b;
+    }
+    elseif($operation == '-'){
+        $result = $a - $b;
+    }
+    elseif ($operation =='*'){
+        $result = $a * $b;
+    }
+    elseif($operation == '/'){
+        $result = $a / $b;
+    }
+    else 
+    echo "ceci n'est pas un operateur";
+    
+    return $result;
+}
+
+echo calcule(3,'-',5);
+?> 
